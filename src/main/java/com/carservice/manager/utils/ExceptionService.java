fa33d6d69901127ca0f1b6fa24991c065fff5a6d
PG <szadowek91@gmail.com>
2022-02-21 14:04:42 +0100

    cleaned code

--- src/main/java/com/carservice/manager/utils/ExceptionService.java
+++
@@ -1,9 +0,0 @@
-package com.carservice.manager.utils;
-
-public class ExceptionService extends RuntimeException{
-
-    public ExceptionService(Long id){
-        super ("Can not find vehicle with selected id: " + id);
-    }
-
-}
