4d683aa985c5af3bb6834ff35268ce0d1701b2ee
PG <szadowek91@gmail.com>
2022-02-21 11:29:42 +0100

    added functionality save and read Json file

--- src/main/java/com/carservice/manager/repository/VehicleRepository.java
+++ src/main/java/com/carservice/manager/repository/VehicleRepository.java
@@ -2,11 +2,13 @@ package com.carservice.manager.repository;
 
 import com.carservice.manager.model.VehicleModel;
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import org.springframework.stereotype.Repository;
 
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Reader;
+import java.lang.reflect.Type;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
@@ -16,44 +18,38 @@ import java.util.List;
 public class VehicleRepository {
 
 
-    private List<VehicleModel> vehicleModels;
-
-
-    public VehicleRepository() {
-        vehicleModels = new ArrayList<>();
-    }
-
     public List<VehicleModel> getVehicleModels() {
-        return vehicleModels;
+        return readDataFromFile();
     }
 
 
     public void addVehicle(VehicleModel vehicleModel) {
-        vehicleModels.add(vehicleModel);
-
         saveDataToFile(vehicleModel);
     }
 
 
     private void saveDataToFile(VehicleModel vehicleModel) {
         Gson gson = new Gson();
-
+        List<VehicleModel> vehicleModels = readDataFromFile();
+        vehicleModels.add(vehicleModel);
         try {
             try (FileWriter fw = new FileWriter("main.json")) {
-                gson.toJson(vehicleModel, fw);
+                gson.toJson(vehicleModels, fw);
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    private VehicleModel readDataFromFile() {
+    private List<VehicleModel> readDataFromFile() {
         Gson gson = new Gson();
         try (Reader reader = Files.newBufferedReader(Paths.get("main.json"))) {
-            return gson.fromJson(reader, VehicleModel.class);
+            Type vehicleType = new TypeToken<ArrayList<VehicleModel>>() {
+            }.getType();
+            return gson.fromJson(reader, vehicleType);
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return null;
+        return new ArrayList<>();
     }
 }
