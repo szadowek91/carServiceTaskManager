53a8fa4db868633ad0782e0c79ba19dca1a9485b
PG <szadowek91@gmail.com>
2022-02-28 13:11:28 +0100

    deleted unused classes

--- src/main/java/com/carservice/manager/utils/DateUtil.java
+++
@@ -1,8 +0,0 @@
-package com.carservice.manager.utils;
-
-import java.text.SimpleDateFormat;
-
-public class DateUtil {
-
-    public static final SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm");
-}
