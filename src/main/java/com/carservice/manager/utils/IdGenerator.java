14a1db401909d1aa3c34a05970afc8121eb90d1f
PG <szadowek91@gmail.com>
2022-03-07 11:27:50 +0100

    added IdGenerator

---
+++ src/main/java/com/carservice/manager/utils/IdGenerator.java
@@ -0,0 +1,15 @@
+package com.carservice.manager.utils;
+
+import java.util.UUID;
+
+public class IdGenerator {
+
+    public static int generateUniqueId() {
+        UUID idOne = UUID.randomUUID();
+        String str = "" + idOne;
+        int uid = str.hashCode();
+        String filterStr = "" + uid;
+        str = filterStr.replaceAll("-", "");
+        return Integer.parseInt(str);
+    }
+}
